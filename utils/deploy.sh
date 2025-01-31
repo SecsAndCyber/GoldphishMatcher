10c9d5971c61a4e5a8c421365773ca98720287ad
Matthew S1air Molyett <matthewmolyett@gmail.com>
2025-01-31 00:09:25 -0500

    Version 1.3.0

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,6 +1,6 @@
 HOST=${HOST:="molyett.com"}
 DEST=${DEST:='~/www/haxe/GoldphishMatcher'}
-VERSION=${VERSION:="1.2.0"}
+VERSION=${VERSION:="1.3.0"}
 PROJECT="GoldphishMatcher"
 
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
