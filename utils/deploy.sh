75fafeea71aa79f8ad93b4ee4b3fdc177a181c36
Matthew Molyett <matthew@molyett.com>
2025-02-03 18:16:16 -0500

    Commit iOS build scripts

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,6 +1,6 @@
 HOST=${HOST:="molyett.com"}
 DEST=${DEST:='~/www/haxe/GoldphishMatcher'}
-VERSION=${VERSION:="1.4.0.1"}
+VERSION=${VERSION:="1.4.1"}
 PROJECT="GoldphishMatcher"
 
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
---
+++ utils/mac_build.sh
@@ -0,0 +1,12 @@
+godotPath="/Applications/Godot.app/Contents/MacOS/Godot"
+
+FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
+ROOT_DIRECTORY="$(realpath "${SCRIPT_DIRECTORY}/../")"
+
+exportIOSDir="$ROOT_DIRECTORY/exports/iOS"
+if [ -d "$exportIOSDir" ]; then
+    rm -rf "${exportIOSDir:?}"/*
+fi
+
+$godotPath --editor --path $ROOT_DIRECTORY --export-release "iOS" $exportIOSDir/GoldphishMatcher.ipa
