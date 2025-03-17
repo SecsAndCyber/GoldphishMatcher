dc633937823da33faea5932b00da56e47fe8fadb
Matthew S1air Molyett <matthewmolyett@gmail.com>
2025-03-16 23:05:18 -0400

    Prepare for continued development

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,6 +1,6 @@
 HOST=${HOST:="molyett.com"}
 DEST=${DEST:='~/www/haxe/GoldphishMatcher'}
-VERSION=${VERSION:="1.8.0"}
+VERSION=${VERSION:="1.8.2"}
 PROJECT="GoldphishMatcher"
 
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
--- utils/mac_deploy.sh
+++ utils/mac_deploy.sh
@@ -1,5 +1,5 @@
 
-VERSION=${VERSION:="1.8.0"}
+VERSION=${VERSION:="1.8.2"}
 PROJECT="GoldphishMatcher"
 
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
