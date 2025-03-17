1d47c76093989ec918bf05b582b09368e7e41049
Matthew S1air Molyett <matthewmolyett@gmail.com>
2025-03-16 21:02:32 -0400

    Add spring flowers

---
+++ utils/update_backgrounds.sh
@@ -0,0 +1,9 @@
+ls /mnt/c/tmp
+cp /mnt/c/tmp/0001.png assets/backgrounds/background-1.png
+cp /mnt/c/tmp/0002.png assets/backgrounds/background-2.png
+cp /mnt/c/tmp/0003.png assets/backgrounds/background-3.png
+cp /mnt/c/tmp/0004.png assets/backgrounds/background-4.png
+cp /mnt/c/tmp/0005.png assets/backgrounds/foreground-1.png
+cp /mnt/c/tmp/0006.png assets/backgrounds/foreground-2.png
+cp /mnt/c/tmp/0007.png assets/backgrounds/foreground-3.png
+cp /mnt/c/tmp/0008.png assets/backgrounds/foreground-4.png
